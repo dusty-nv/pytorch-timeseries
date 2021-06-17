f52ac07f92e20a5aa1be0a596dc91fa91428042a
Dustin Franklin <dustinf@nvidia.com>
2021-06-17 16:13:12 -0400

    updated weather script

--- scripts/prepare_weather.py
+++ scripts/prepare_weather.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import os
 import argparse
 import pandas as pd
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input', default='data/weather_data/temperature.csv', type=str)
+parser.add_argument('-i', '--input', default='data/weather_data/', type=str)
 parser.add_argument('-o', '--output', default='data/weather.csv', type=str)
 parser.add_argument('--city', default='Vancouver', type=str)
 args = parser.parse_args()
 
 
-df = pd.read_csv(args.input, usecols=['datetime', args.city], parse_dates=[0])
-#print(df)
+df = None
+attributes = ['temperature', 'humidity', 'pressure', 'wind_direction', 'wind_speed']
 
-df = df.dropna()
-#print(df)
+for attribute in attributes:
+    df_attr = pd.read_csv(os.path.join(args.input, attribute + '.csv'), usecols=['datetime', args.city], index_col=0, parse_dates=[0])
+    df_attr.rename(columns={args.city:attribute}, inplace=True)
+    
+    if df is None:
+        df = df_attr
+    else:
+        df = pd.merge(df, df_attr, how='inner', left_index=True, right_index=True)
 
-#df['day'] = df['datetime'].dt.dayofyear
-#df['hour'] = df['datetime'].dt.hour
-
+df.dropna(inplace=True)
+        
 # convert kelvin to fahrenheit
-df[args.city] = (df[args.city] - 273.15) * (9/5) + 32.0
-
-df.rename(columns={args.city:'temperature'}, inplace=True)
+df['temperature'] = (df['temperature'] - 273.15) * (9/5) + 32.0
 print(df)
 
-df.to_csv(args.output, index=False)
+df.to_csv(args.output)
 print(f"saved {len(df)} rows to {args.output}")
